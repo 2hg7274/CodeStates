2d95eb40e9c95e9dcf3eff487d9224ad9657671a
Hyun Gu Han/한현구 <94213374+2hg7274@users.noreply.github.com>
2022-07-26 16:56:23 +0900

    section4 project model (1차 모델- 아직 수정 중)

---
+++ Section4 Project/models/cnn.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Flatten,Dense,Dropout,BatchNormalization
+from keras import regularizers
+from tensorflow.keras.optimizers import Adam,RMSprop,SGD,Adamax
+
+def cnn(input_shape, num_classes):
+    model= tf.keras.models.Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', input_shape=input_shape))
+    model.add(Conv2D(64,(3,3), padding='same', activation='relu' ))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128,(5,5), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(512,(3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(512,(3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten()) 
+    model.add(Dense(256,activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+
+    model.add(Dense(512,activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(
+        optimizer = Adam(lr=0.0001), 
+        loss='categorical_crossentropy', 
+        metrics=['accuracy']
+    )
+    return model
+
+
+def fine_tunning(model):
+    model.layers[1].trainable = True
+
+    model.compile(optimizer=tf.keras.optimizers.SGD(0.001), #lower learning rate
+                    loss='categorical_crossentropy',
+                    metrics = ['accuracy'])
+
+    return model
